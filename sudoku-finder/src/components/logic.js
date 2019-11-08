9eec75fcc94e8ee1dd217b404bac551246f1bfc7
akhilomar <akhilomar555@gmail.com>
2019-11-08 22:08:32 +0530

    logic uncommented

--- sudoku-finder/src/components/logic.js
+++ sudoku-finder/src/components/logic.js
@@ -18,60 +18,60 @@ const Solver=props=>{
     [props.values.r70, props.values.r71, props.values.r72, props.values.r73, props.values.r74, props.values.r75, props.values.r76, props.values.r77, props.values.r78],
     [props.values.r80, props.values.r81, props.values.r82, props.values.r83, props.values.r84, props.values.r85, props.values.r86, props.values.r87, props.values.r88]];             
    
-    
+
 
     
    useEffect(()=>{
     console.log(matrix)
    }
    )
-    // var nextIndex=(board)=>{    
-    // for(let i=0;i<board.length;i++)
-    // {
-    //     for(let j=0;j<board[0].length;j++){
-    //         if(board[i][j]==="")
-    //         return [i,j];
-    //     }
-    // }
-    // return false;
-    // }
+    var nextIndex=(board)=>{    
+    for(let i=0;i<board.length;i++)
+    {
+        for(let j=0;j<board[0].length;j++){
+            if(board[i][j]==="")
+            return [i,j];
+        }
+    }
+    return false;
+    }
 
-    // var isValidNum=(board,row,col,num)=>{
-    //     for(let j=0;j<board[0].length;j++)
-    //         if(board[row][j]===num && j!==col)
-    //             return false;
-    //     for(let i=0;i<board.length; i++)
-    //         if(board[i][col]===num && i!=col){
-    //             return false;
-    //         }
-    // let boxRow=parseInt(row/3)*3;
-    // let boxCol=parseInt(col/3)*3;
-    // for(let i=boxRow;i<boxRow+3;i++){
-    //     for(let j=boxCol;j<boxCol+3;j++){
-    //         if(board[i][j]===num && (i!==row && j!==col)){
-    //             return false;
-    //         }
-    //     }
-    // }
-    //     return true;
-    // }
+    var isValidNum=(board,row,col,num)=>{
+        for(let j=0;j<board[0].length;j++)
+            if(board[row][j]===num && j!==col)
+                return false;
+        for(let i=0;i<board.length; i++)
+            if(board[i][col]===num && i!=col){
+                return false;
+            }
+    let boxRow=parseInt(row/3)*3;
+    let boxCol=parseInt(col/3)*3;
+    for(let i=boxRow;i<boxRow+3;i++){
+        for(let j=boxCol;j<boxCol+3;j++){
+            if(board[i][j]===num && (i!==row && j!==col)){
+                return false;
+            }
+        }
+    }
+        return true;
+    }
 
-    // var solveSudoku=(board)=>{
-    //     if(!nextIndex(board))
-    //         return true;
-    //     let boardIndexes=nextIndex(board);
-    //     let row=boardIndexes[0];
-    //     let col=boardIndexes[1];
-    //     for(let num=1;num<10;num++)
-    //         if(isValidNum(board,row,col,num)){
-    //             board[row][col]=num+'';
-    //             if(solveSudoku(board)){
-    //                 return true;
-    //             }
-    //         }
-    // }
+    var solveSudoku=(board)=>{
+        if(!nextIndex(board))
+            return true;
+        let boardIndexes=nextIndex(board);
+        let row=boardIndexes[0];
+        let col=boardIndexes[1];
+        for(let num=1;num<10;num++)
+            if(isValidNum(board,row,col,num)){
+                board[row][col]=num+'';
+                if(solveSudoku(board)){
+                    return true;
+                }
+            }
+    }
 
-    // solveSudoku(matrix);
+    solveSudoku(matrix);
 
     
 let content=(
